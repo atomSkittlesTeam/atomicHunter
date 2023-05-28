905ba73a1e75ee6e508b82bc696d1407d7eb84e9
Artem Sidorov <artemsrv3@gmail.com>
2023-05-28 11:04:50 +0300

    email with attachment

--- atomichunter-rest/src/main/java/net/vniia/skittles/services/EmailService.java
+++ atomichunter-rest/src/main/java/net/vniia/skittles/services/EmailService.java
@@ -1,5 +1,11 @@
 package net.vniia.skittles.services;
 
+import jakarta.mail.MessagingException;
+
+import java.io.FileNotFoundException;
+
 public interface EmailService {
     void sendSimpleMessage(String to, String subject, String text);
+    void sendEmailWithAttachment(String toAddress, String subject, String message, String attachment)
+            throws MessagingException, FileNotFoundException;
 }
--- atomichunter-rest/src/main/java/net/vniia/skittles/services/EmailServiceImpl.java
+++ atomichunter-rest/src/main/java/net/vniia/skittles/services/EmailServiceImpl.java
@@ -1,15 +1,23 @@
 package net.vniia.skittles.services;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import java.io.FileNotFoundException;
 
 @Service
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
     private final JavaMailSender emailSender;
 
+    @Override
     public void sendSimpleMessage(
             String to, String subject, String text) {
         try {
@@ -24,5 +32,21 @@ public class EmailServiceImpl implements EmailService {
         } catch (Exception e) {
             return;
         }
+    }
+
+
+    @Override
+    public void sendEmailWithAttachment(String toAddress, String subject, String message, String attachment)
+            throws MessagingException, FileNotFoundException {
+
+        MimeMessage mimeMessage = emailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true);
+        messageHelper.setFrom("atomskittles@rambler.ru");
+        messageHelper.setTo(toAddress);
+        messageHelper.setSubject(subject);
+        messageHelper.setText(message);
+        FileSystemResource file = new FileSystemResource(ResourceUtils.getFile(attachment));
+        messageHelper.addAttachment("Purchase Order", file);
+        emailSender.send(mimeMessage);
     }
 }
