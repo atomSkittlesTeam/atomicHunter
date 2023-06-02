8065a1d0bb4dc691071fc95b42ac37152440d64d
Artem Sidorov <artemsrv3@gmail.com>
2023-06-02 21:04:33 +0300

    поправил инструкцию и вынес настройки мыла в yml

--- atomichunter-rest/src/main/java/net/vniia/skittles/services/EmailServiceImpl.java
+++ atomichunter-rest/src/main/java/net/vniia/skittles/services/EmailServiceImpl.java
@@ -3,10 +3,7 @@ package net.vniia.skittles.services;
 import biweekly.Biweekly;
 import biweekly.ICalendar;
 import biweekly.component.VEvent;
-import biweekly.parameter.ParticipationLevel;
-import biweekly.property.Attendee;
 import biweekly.property.Method;
-import biweekly.property.Organizer;
 import biweekly.util.Duration;
 import jakarta.activation.DataHandler;
 import jakarta.mail.Message;
@@ -18,7 +15,6 @@ import jakarta.mail.internet.MimeMultipart;
 import jakarta.mail.util.ByteArrayDataSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import net.vniia.skittles.configs.MailConfiguration;
 import net.vniia.skittles.entities.ConfirmationToken;
 import net.vniia.skittles.repositories.ConfirmationTokenRepository;
 import org.springframework.beans.factory.annotation.Value;
@@ -39,6 +35,10 @@ import java.util.Objects;
 @Log4j2
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
+
+    @Value("${spring.mail.username}")
+    private String serviceMail;
+
     private final JavaMailSender emailSender;
 
     private final ConfirmationTokenRepository confirmationTokenRepository;
@@ -56,7 +56,7 @@ public class EmailServiceImpl implements EmailService {
             String to, String subject, String text) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(MailConfiguration.SERVICE_EMAIL);
+            message.setFrom(serviceMail);
             message.setTo(to);
             message.setSubject(subject);
             message.setText(text);
@@ -74,7 +74,7 @@ public class EmailServiceImpl implements EmailService {
         MimeMessage mimeMessage = emailSender.createMimeMessage();
         MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true);
 
-        InternetAddress sender = new InternetAddress(MailConfiguration.SERVICE_EMAIL, "Atomic Hunter");
+        InternetAddress sender = new InternetAddress(serviceMail, "Atomic Hunter");
         InternetAddress consumer = new InternetAddress(toAddress);
 
         mimeMessage.setRecipient(Message.RecipientType.TO, consumer);
@@ -124,7 +124,7 @@ public class EmailServiceImpl implements EmailService {
     public void sendCalendarInvite(String subject, String consumerEmail, Long vacancyRespondId) throws Exception {
 
         MimeMessage mimeMessage = emailSender.createMimeMessage();
-        InternetAddress sender = new InternetAddress(MailConfiguration.SERVICE_EMAIL, "Atomic Hunter");
+        InternetAddress sender = new InternetAddress(serviceMail, "Atomic Hunter");
         InternetAddress consumer = new InternetAddress(consumerEmail);
 
 //        mimeMessage.addHeaderLine("method=PUBLISH");
