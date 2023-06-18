3cfa39666a5f286de1c6ad3904c28ae8ff6ff1ca
user91 <user91@as.ru>
2023-06-18 10:46:53 +0500

    req to back

--- atomichunter-web/src/app/services/vacancy.service.ts
+++ atomichunter-web/src/app/services/vacancy.service.ts
@@ -1,93 +1,99 @@
-import { Injectable } from '@angular/core';
-import { BaseService } from './base.service';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { ConfigService } from '../config/config.service';
-import { firstValueFrom } from 'rxjs';
-import { Vacancy } from '../dto/Vacancy';
+import {Injectable} from '@angular/core';
+import {BaseService} from './base.service';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {ConfigService} from '../config/config.service';
+import {firstValueFrom} from 'rxjs';
+import {Vacancy} from '../dto/Vacancy';
 import {VacancyRespond} from "../dto/VacancyRespond";
+import {VacancyCompetenceScoreRequestDto} from "../dto/VacancyCompetenceScoreRequestDto";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class VacancyService extends BaseService {
 
-  constructor(private http: HttpClient,
-    public router: Router,
-    public override configService: ConfigService) {
-    super(configService);
-  }
+    constructor(private http: HttpClient,
+                public router: Router,
+                public override configService: ConfigService) {
+        super(configService);
+    }
 
-  async getVacancies(showArchive: boolean) {
-    const url = await this.getBackendUrl();
-    return await firstValueFrom(this.http.get<Vacancy[]>(url + '/vacancy/all',
-        {
-            params: {
-                showArchive: showArchive
+    async getVacancies(showArchive: boolean) {
+        const url = await this.getBackendUrl();
+        return await firstValueFrom(this.http.get<Vacancy[]>(url + '/vacancy/all',
+            {
+                params: {
+                    showArchive: showArchive
+                }
             }
-        }
-    ));
-  }
+        ));
+    }
 
-  async getVacancyById(id: number) {
-    const url = await this.getBackendUrl();
-    return await firstValueFrom(this.http.get<Vacancy>(url + `/vacancy/${id}`));
-  }
+    async getVacancyById(id: number) {
+        const url = await this.getBackendUrl();
+        return await firstValueFrom(this.http.get<Vacancy>(url + `/vacancy/${id}`));
+    }
 
-  async createVacancy(vacancy: Vacancy) {
-    const url = await this.getBackendUrl();
-    return await firstValueFrom(this.http.post(url + '/vacancy/create', vacancy));
-  }
+    async createVacancy(vacancy: Vacancy) {
+        const url = await this.getBackendUrl();
+        return await firstValueFrom(this.http.post(url + '/vacancy/create', vacancy));
+    }
 
-  async updateVacancy(id: number, vacancy: Vacancy) {
-    const url = await this.getBackendUrl();
-    return await firstValueFrom(this.http.put(url + `/vacancy/${id}/update`, vacancy));
-  }
-  
-  async archiveVacancy(id: number) {
-    const url = await this.getBackendUrl();
-    await firstValueFrom(this.http.delete(url + `/vacancy/${id}/archive`));
-  }
+    async updateVacancy(id: number, vacancy: Vacancy) {
+        const url = await this.getBackendUrl();
+        return await firstValueFrom(this.http.put(url + `/vacancy/${id}/update`, vacancy));
+    }
 
-  async getVacancyRespondsByIds(ids: number[], showArchive: boolean) {
-    const url = await this.getBackendUrl();
-    return await firstValueFrom(this.http.post<Vacancy[]>(url + '/vacancy/respond/get-all-by-ids', ids,
-      {
-        params: {
-          showArchive: showArchive
-        }
-      }
-    ));
-  }
+    async archiveVacancy(id: number) {
+        const url = await this.getBackendUrl();
+        await firstValueFrom(this.http.delete(url + `/vacancy/${id}/archive`));
+    }
 
-  async getVacancyRespondById(id: number) {
-    const url = await this.getBackendUrl();
-    return await firstValueFrom(this.http.get<VacancyRespond>(url + `/vacancy/respond/${id}`));
-  }
+    async getVacancyRespondsByIds(ids: number[], showArchive: boolean) {
+        const url = await this.getBackendUrl();
+        return await firstValueFrom(this.http.post<Vacancy[]>(url + '/vacancy/respond/get-all-by-ids', ids,
+            {
+                params: {
+                    showArchive: showArchive
+                }
+            }
+        ));
+    }
 
-  async createVacancyRespond(vacancyRespond: VacancyRespond) {
-    const url = await this.getBackendUrl();
-    return await firstValueFrom(this.http.post(url + '/vacancy/respond/create', vacancyRespond));
-  }
+    async validateVacancyCompetenceScore(vacancyCompScoreReq: VacancyCompetenceScoreRequestDto) {
+        const url = await this.getBackendUrl();
+        return await firstValueFrom(this.http.post<VacancyCompetenceScoreRequestDto>(url + '/vacancy/competence-score/add', vacancyCompScoreReq));
+    }
 
-  async updateVacancyRespond(vacancyRespondId: number, vacancyRespond: VacancyRespond) {
-    const url = await this.getBackendUrl();
-    return await firstValueFrom(this.http.put(url + `/vacancy/respond/${vacancyRespondId}/update`, vacancyRespond));
-  }
+    async getVacancyRespondById(id: number) {
+        const url = await this.getBackendUrl();
+        return await firstValueFrom(this.http.get<VacancyRespond>(url + `/vacancy/respond/${id}`));
+    }
 
-  async archiveVacancyRespond(vacancyRespondId: number) {
-    const url = await this.getBackendUrl();
-    await firstValueFrom(this.http.delete(url + `/vacancy/respond/${vacancyRespondId}/archive`));
-  }
+    async createVacancyRespond(vacancyRespond: VacancyRespond) {
+        const url = await this.getBackendUrl();
+        return await firstValueFrom(this.http.post(url + '/vacancy/respond/create', vacancyRespond));
+    }
 
-  async createVacancyReport(vacancyId: number, additionalInformationForReport: string) {
-    const url = await this.getBackendUrl();
-    return await firstValueFrom(this.http.post<string[]>(
-        url + `/vacancy/${vacancyId}/report`, additionalInformationForReport));
-  }
+    async updateVacancyRespond(vacancyRespondId: number, vacancyRespond: VacancyRespond) {
+        const url = await this.getBackendUrl();
+        return await firstValueFrom(this.http.put(url + `/vacancy/respond/${vacancyRespondId}/update`, vacancyRespond));
+    }
 
-  async getVacancyFileReport(vacancyId: number, path: string) {
-    const url = await this.getBackendUrl();
-    window.open(url + `/vacancy/${vacancyId}/report/${path}/filePdf`, '_blank');
-  }
+    async archiveVacancyRespond(vacancyRespondId: number) {
+        const url = await this.getBackendUrl();
+        await firstValueFrom(this.http.delete(url + `/vacancy/respond/${vacancyRespondId}/archive`));
+    }
+
+    async createVacancyReport(vacancyId: number, additionalInformationForReport: string) {
+        const url = await this.getBackendUrl();
+        return await firstValueFrom(this.http.post<string[]>(
+            url + `/vacancy/${vacancyId}/report`, additionalInformationForReport));
+    }
+
+    async getVacancyFileReport(vacancyId: number, path: string) {
+        const url = await this.getBackendUrl();
+        window.open(url + `/vacancy/${vacancyId}/report/${path}/filePdf`, '_blank');
+    }
 }
