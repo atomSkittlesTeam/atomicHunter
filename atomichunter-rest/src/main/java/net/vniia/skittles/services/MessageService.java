2e0cfd6d1f4c896725f7a160e51d896fd291c5c1
Artem Sidorov <artemsrv3@gmail.com>
2023-07-11 22:07:30 +0300

    comment notify

--- atomichunter-rest/src/main/java/net/vniia/skittles/services/MessageService.java
+++ atomichunter-rest/src/main/java/net/vniia/skittles/services/MessageService.java
@@ -65,54 +65,56 @@ public class MessageService {
     }
 
     public List<MessageDto> getNewMessagesForFrontendByLogin(String userLogin) {
-        List<Message> newMessages = queryFactory
-                .from(message)
-                .select(message)
-                .where(message.login.eq(userLogin)
-                        .and(message.frontSign.eq(false)
-                                .or(message.frontSign.isNull())))
-                .fetch();
-        return newMessages.stream().map(MessageDto::new).toList();
+//        List<Message> newMessages = queryFactory
+//                .from(message)
+//                .select(message)
+//                .where(message.login.eq(userLogin)
+//                        .and(message.frontSign.eq(false)
+//                                .or(message.frontSign.isNull())))
+//                .fetch();
+//        return newMessages.stream().map(MessageDto::new).toList();
+        return new ArrayList<>();
     }
 
     @Transactional
     public void setMessageReadFromFrontend(List<Long> messageIds) {
-        List<Message> messages = messageRepository.findAllById(messageIds);
-        messages.forEach(e -> e.setFrontSign(true));
-        messageRepository.saveAll(messages);
-        messageRepository.flush();
+//        List<Message> messages = messageRepository.findAllById(messageIds);
+//        messages.forEach(e -> e.setFrontSign(true));
+//        messageRepository.saveAll(messages);
+//        messageRepository.flush();
     }
 
-    @Scheduled(fixedDelay = 10000)
+//    @Scheduled(fixedDelay = 10000)
     public void scheduledSendEmailMessages() {
-        List<Message> newMessages = queryFactory.select(message)
-                .from(message)
-                .where(message.emailSign.isFalse())
-                .fetch();
-
-        newMessages.forEach(message -> {
-            UserDto user = userService.getUser(message.getLogin());
-            emailService.sendSimpleMessage(user.getEmail(), message.getObjectName(), message.getObjectName());
-        });
-
-        newMessages.forEach(e -> e.setEmailSign(true));
-        messageRepository.saveAll(newMessages);
+//        List<Message> newMessages = queryFactory.select(message)
+//                .from(message)
+//                .where(message.emailSign.isFalse())
+//                .fetch();
+//
+//        newMessages.forEach(message -> {
+//            UserDto user = userService.getUser(message.getLogin());
+//            emailService.sendSimpleMessage(user.getEmail(), message.getObjectName(), message.getObjectName());
+//        });
+//
+//        newMessages.forEach(e -> e.setEmailSign(true));
+//        messageRepository.saveAll(newMessages);
 //        System.out.println("Отправил сообщения на почты, по шедулеру");
     }
 
     public Message createMessageByLogin(String login, String messageText, Long objectId) {
-        User user = userRepository.findByLogin(login);
-        if (user != null) {
-            Message message = new Message(user.login,
-                    false,
-                    false,
-                    objectId,
-                    messageText);
-            message = messageRepository.save(message);
-            return message;
-        } else {
-            throw new RuntimeException("User не найден");
-        }
+//        User user = userRepository.findByLogin(login);
+//        if (user != null) {
+//            Message message = new Message(user.login,
+//                    false,
+//                    false,
+//                    objectId,
+//                    messageText);
+//            message = messageRepository.save(message);
+//            return message;
+//        } else {
+//            throw new RuntimeException("User не найден");
+//        }
+        return new Message();
     }
 
     public List<Message> createMessagesByRole(String role, Long objectId, String messageText) {
@@ -127,67 +129,68 @@ public class MessageService {
     }
 
     public List<Message> createMessagesForAllUsers(Long objectId, String messageText) {
-        List<Message> messageList = new ArrayList<>();
-        List<User> actualUsers = userRepository.findAll();
-        for (User user : actualUsers) {
-            Message message = new Message(user.login, false, false, objectId, messageText);
-            messageList.add(message);
-        }
-        messageList = messageRepository.saveAll(messageList);
-        return messageList;
+//        List<Message> messageList = new ArrayList<>();
+//        List<User> actualUsers = userRepository.findAll();
+//        for (User user : actualUsers) {
+//            Message message = new Message(user.login, false, false, objectId, messageText);
+//            messageList.add(message);
+//        }
+//        messageList = messageRepository.saveAll(messageList);
+//        return messageList;
+        return new ArrayList<>();
     }
 
     public void createTelegramMessageByRole(String role, String message) {
-        if (isTelegramEnabled()) {
-            List<TelegramSubscriber> usersWithRole = telegramSubscriberRepository.findAllByRole(role);
-            if (!usersWithRole.isEmpty()) {
-                List<TelegramMailingHistory> historiesForRoleMailing = new ArrayList<>();
-                for (TelegramSubscriber user : usersWithRole) {
-                    TelegramMailingHistory mailToUser = new TelegramMailingHistory();
-                    mailToUser.setMailingType(TelegramMailingType.ROLE);
-                    mailToUser.setMailingTo(user.getRole());
-                    mailToUser.setMessage(message);
-                    mailToUser.setMessageDate(Instant.now());
-                    mailToUser.setProcessed(false);
-                    historiesForRoleMailing.add(mailToUser);
-                }
-                telegramMailingHistoryRepository.saveAll(historiesForRoleMailing);
-            }
-        }
+//        if (isTelegramEnabled()) {
+//            List<TelegramSubscriber> usersWithRole = telegramSubscriberRepository.findAllByRole(role);
+//            if (!usersWithRole.isEmpty()) {
+//                List<TelegramMailingHistory> historiesForRoleMailing = new ArrayList<>();
+//                for (TelegramSubscriber user : usersWithRole) {
+//                    TelegramMailingHistory mailToUser = new TelegramMailingHistory();
+//                    mailToUser.setMailingType(TelegramMailingType.ROLE);
+//                    mailToUser.setMailingTo(user.getRole());
+//                    mailToUser.setMessage(message);
+//                    mailToUser.setMessageDate(Instant.now());
+//                    mailToUser.setProcessed(false);
+//                    historiesForRoleMailing.add(mailToUser);
+//                }
+//                telegramMailingHistoryRepository.saveAll(historiesForRoleMailing);
+//            }
+//        }
     }
 
     public void createTelegramMessageByLogin(String login, String message) {
-        if (isTelegramEnabled()) {
-            TelegramSubscriber subscriber = this.telegramSubscriberRepository.findByLogin(login);
-            if (subscriber != null) {
-                TelegramMailingHistory mailToSubscriber = new TelegramMailingHistory();
-                mailToSubscriber.setMailingType(TelegramMailingType.LOGIN);
-                mailToSubscriber.setMailingTo(subscriber.getLogin());
-                mailToSubscriber.setMessage(message);
-                mailToSubscriber.setMessageDate(Instant.now());
-                mailToSubscriber.setProcessed(false);
-                this.telegramMailingHistoryRepository.save(mailToSubscriber);
-            }
-        }
+//        if (isTelegramEnabled()) {
+//            TelegramSubscriber subscriber = this.telegramSubscriberRepository.findByLogin(login);
+//            if (subscriber != null) {
+//                TelegramMailingHistory mailToSubscriber = new TelegramMailingHistory();
+//                mailToSubscriber.setMailingType(TelegramMailingType.LOGIN);
+//                mailToSubscriber.setMailingTo(subscriber.getLogin());
+//                mailToSubscriber.setMessage(message);
+//                mailToSubscriber.setMessageDate(Instant.now());
+//                mailToSubscriber.setProcessed(false);
+//                this.telegramMailingHistoryRepository.save(mailToSubscriber);
+//            }
+//        }
     }
 
     public void createTelegramMessagesForAllUsers(String message) {
-        if (isTelegramEnabled()) {
-            List<TelegramSubscriber> subscribers = this.telegramSubscriberRepository.findAll();
-            if (!subscribers.isEmpty()) {
-                List<TelegramMailingHistory> messages = new ArrayList<>();
-                for (TelegramSubscriber subscriber : subscribers) {
-                    TelegramMailingHistory mailToSubscriber = new TelegramMailingHistory();
-                    mailToSubscriber.setMailingType(TelegramMailingType.LOGIN);
-                    mailToSubscriber.setMailingTo(subscriber.getLogin());
-                    mailToSubscriber.setMessage(message);
-                    mailToSubscriber.setMessageDate(Instant.now());
-                    mailToSubscriber.setProcessed(false);
-                    messages.add(mailToSubscriber);
-                }
-                this.telegramMailingHistoryRepository.saveAll(messages);
-            }
-        }
+//        if (isTelegramEnabled()) {
+//            List<TelegramSubscriber> subscribers = this.telegramSubscriberRepository.findAll();
+//            if (!subscribers.isEmpty()) {
+//                List<TelegramMailingHistory> messages = new ArrayList<>();
+//                for (TelegramSubscriber subscriber : subscribers) {
+//                    TelegramMailingHistory mailToSubscriber = new TelegramMailingHistory();
+//                    mailToSubscriber.setMailingType(TelegramMailingType.LOGIN);
+//                    mailToSubscriber.setMailingTo(subscriber.getLogin());
+//                    mailToSubscriber.setMessage(message);
+//                    mailToSubscriber.setMessageDate(Instant.now());
+//                    mailToSubscriber.setProcessed(false);
+//                    messages.add(mailToSubscriber);
+//                }
+//                this.telegramMailingHistoryRepository.saveAll(messages);
+//            }
+//        }
     }
 
     private boolean isTelegramEnabled() {
